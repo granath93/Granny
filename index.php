4e68dde407d1b4a17281f4337e374f93461f67f6
granath93 <rysaren@hotmail.com>
2014-04-15 11:06:19 +0200

    uppdaterat

--- index.php
+++ index.php
@@ -1,1 +1,24 @@
+<?php
+include("database.php");
+
+$query = <<<END
+SELECT *
+FROM article;
+END;
+
+//Exekutiverar "verkställer" SELECT-satsen
+$res = $mysqli->query($query) or die("Could not query database" . $mysqli->errno . 
+" : " . $mysqli->error); //Performs query
+
+while($row = $res->fetch_object()) { 
+$ett = ($row->ArticleId); 
+$tva = ($row->ArtName); 
+$tre = ($row->Price);
+
+echo $ett . $tva . $tre;
+
+}
+?>
+
 hej
+
