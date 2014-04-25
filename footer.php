3f8af891fd09363a23518cf0b1a6adb60bf07a1b
granath93 <rysaren@hotmail.com>
2014-04-25 14:43:07 +0200

    nu funkar footern

--- footer.php
+++ footer.php
@@ -8,12 +8,12 @@
 
 
 		<div class="footerContent">
-			<a href="#"><button><p>Om oss</p></button></a><br><br>
+			<a href="about.php"><button><p>Om oss</p></button></a><br><br>
 			<p>Under denna flik kan du <br> läsa om företaget</p>
 		</div>
 
 		<div class="footerContent">
-			<a href="#"><button><p>Kontakta oss</p></button></a><br><br>
+			<a href="about.php"><button><p>Kontakta oss</p></button></a><br><br>
 			<p><b>Adress:</b> Fågelgatan 32<br>
 			<b>Postnr/ort: </b> 30266 Halmstad<br>
 			<b>Telefonr: </b> 035-86245</p>
