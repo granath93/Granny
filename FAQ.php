c1611c124e869180a12e16e25bbd64a578df8144
granath93 <rysaren@hotmail.com>
2014-05-06 12:05:16 +0200

    uppdaterat footer, lagt till FAQ,

---
+++ FAQ.php
@@ -0,0 +1,42 @@
+<?php 
+
+include("header.php");
+include("database.php");
+
+?>
+
+<div class="content">
+<div class="FAQ">
+
+<h2>Här hittar du vanliga fårgor och svar om vår hemsida</h2>
+<br>
+
+<h3>Hur lägger jag varor i kundvagnen?</h3>
+<p>Du klickar på knappen "LÄGG I KUNDVAGN" som du hittar intill varje produkt.</p>
+
+<hr> 
+
+<h3>Hur betalar jag?</h3>
+<p>Du går in på "VARUKORG" och klickar på knappen "BETALA" för att gå vidare i betalprocessen.</p>
+
+<hr> 
+
+<h3>Hur loggar jag in?</h3>
+<p>Du klickar på knappen "LOGGA IN" i menyn och skriver in din email som du registrerade dig med och det lösenord du skrev in samtidigt.</p>
+
+<hr> 
+
+<h3>Hur blir jag medlem?</h3>
+<p>Du klickar på knappen "BLI MEDLEM" i menyn och skriver in en email som du kommer att använda som användarnamn samt ett lösenord för att senare kunna logga in. 
+Du skriver även in personuppgifter om vart du bor så att varorna du har köpt kommer hem till dig.</p>
+
+<h3></h3>
+<p></p>
+
+
+</div>
+
+</div>
+
+
+<?php include("footer.php"); ?>
--- footer.php
+++ footer.php
@@ -8,12 +8,12 @@
 
 
 		<div class="footerContent">
-			<a href="about.php"><button><p>Om oss</p></button></a><br><br>
+			<a href="about.php"><button><p>OM OSS</p></button></a><br><br>
 			<p>Under denna flik kan du <br> läsa om företaget</p>
 		</div>
 
 		<div class="footerContent">
-			<a href="about.php"><button><p>Kontakta oss</p></button></a><br><br>
+			<a href="about.php"><button><p>KONTAKTA OSS</p></button></a><br><br>
 			<p><b>Adress:</b> Fågelgatan 32<br>
 			<b>Postnr/ort: </b> 30266 Halmstad<br>
 			<b>Telefonr: </b> 035-86245</p>
@@ -21,7 +21,7 @@
 		</div>
 
 		<div class="footerContent">
-			<a href="#"><button><p>Vanliga frågor och svar</p></button></a><br><br>
+			<a href="FAQ.php"><button><p>VANLIGA FRÅGOR OCH SVAR</p></button></a><br><br>
 			<p> Här får du svar på frågor <br> angående webbutiken <br>och leveranser</p>
 		</div>
 
