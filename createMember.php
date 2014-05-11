a4966d3b1dafd5082abe9488bcde0b1f8216d758
JosefinLiback <josefin.liback@hotmail.com>
2014-05-11 17:18:56 +0200

    Fungerande createMemeber!
    Går också att uppdatera sina uppgifter, dock bara om man manuellt anger
    customerID i koden.

--- createMember.php
+++ createMember.php
@@ -2,35 +2,13 @@
 
 include("database.php");
 include("header.php"); 
-$tableCustomer = "customer";
-?>
 
-<div class="content">
+$feedback = '';
+ 
+if(!empty($_POST)) {
 
-<br></br>
-<p style="text-align:center;">Här fyller du i dina uppgifter. Sedan är du redo att börja handla!</p><br>
+		$table = "customer";
 		
-		<form action="createMember.php" method="post" id="input.form">
-			<p><label for="Fname" for ="Fname">Förnamn:</label></p>
-			  <input type="text" type="text" id="Fname" name="Fname" value="" /><br><br>
-			<p><label for="Lname" for ="Lname">Efternamn:</label></p>
-			 <input type="text" id="Lname" name="Lname" value=""/><br><br>
-			 <p><label for="Address" for ="Address">Adress:</label></p>
-			 <input type="text" id="Address" name="Address" value=""/><br><br>
-			 <p><label for="Zip" for ="Zip">Postnummer:</label></p>
-			 <input type="text" id="Zip" name="Zip" value=""/><br><br>
-			 <p><label for="Email" for ="Email">Din E-post:</label></p>
-			 <input type="text" id="Email" name="Email" value=""/><br><br>
-			 <p><label for="password" for ="password">Ditt lösenord:</label></p>
-			 <input type="password" id="password" name="password" value=""/><br><br>
-			<button class="login" id="login"><p>Klar</p></button>
-		</form>
-
-	</div>
-<?php
-
-
-	if(!empty($_POST)) {
 		$Fname =		isset($_POST['Fname']) ? $_POST['Fname'] : ''; 
 		$Lname =		isset($_POST['Lname']) ? $_POST['Lname'] : '';
 		$Address =		isset($_POST['Address']) ? $_POST['Address'] : '';
@@ -38,43 +16,62 @@ $tableCustomer = "customer";
 		$Email =		isset($_POST['Email']) ? $_POST['Email'] : ''; 
 		$Password =		isset($_POST['Password']) ? $_POST['Password'] : '';
 		
-		foreach($_POST as $value) {
-			if(stripos($value, 'Content-Type:') !== FALSE) {
-				echo "There was a problem. Try again.";
-				exit;
-			}
-		}	
+			if($Fname == '' || $Lname == '' || $Address == '' || $Zip == '' || $Email == '' || $Password == ''){
+				$feedback = "Du missade att fylla i ett fält";
+			} 
+				else{
+				$Fname 		= utf8_encode($mysqli->real_escape_string($Fname)); 
+				$Lame 		= utf8_encode($mysqli->real_escape_string($Lname)); 
+				$Address 	= utf8_encode($mysqli->real_escape_string($Address)); 
+				$Zip 		= utf8_encode($mysqli->real_escape_string($Zip)); 
+				$Email		= utf8_encode($mysqli->real_escape_string($Email)); 
+				$Password 	= utf8_encode($mysqli->real_escape_string($Password)); 
 		
-		if($Fname == '' || $Lname == '' || $Address == '' || $Zip == '' || $Email == ''|| $Password == ''){
-			$content = <<<END
-				<div id="content">
-				<p>Please fill out all fields.</p>
-				</div><!-- container -->
-END;
-	
-		} else {
-			// SQL-injections
-			$Fname = utf8_encode($mysqli->real_escape_string($Fname)); 
-			$Lame = utf8_encode($mysqli->real_escape_string($Lname)); 
-			$Address = utf8_encode($mysqli->real_escape_string($Address)); 
-			$Zip = utf8_encode($mysqli->real_escape_string($Zip)); 
-			$Email = utf8_encode($mysqli->real_escape_string($Email)); 
-			$password = utf8_encode($mysqli->real_escape_string($password)); 
-
-		
-		//SQL query
 		$query = <<<END
-		--
-		-- Insert new customer into DB
-		--
-		INSERT INTO {$tableCustomer} (Fname, Lname, Address, Zip, Email, Password)
-		VALUES ('{$Fname}, '{$Lname}', '{$Address}', '{$Zip}', '{$Email}', '{$Password}');	
+
+		INSERT INTO customer(Fname,Lname,Address,Zip,Email,Password) 
+		VALUES ("{$Fname}","{$Lname}","{$Address}","{$Zip}","{$Email}","{$Password}")
 END;
+
+	$mysqli->query($query) or die("Could not query database" . $mysqli->errno . " : " . $mysqli->error); //Performs query 
+	$feedback = "Your post has been added";
+	header("Location: login.php");
 	
-		
-		$feedback = "<p> Du kan nu logga in! </p>";
-		header("Location: index.php");
-}
 }
 
-include("footer.php"); ?>
+
+$mysqli->close();
+} else {
+	
+	}
+
+?>
+
+<div class="messageContent">
+	<?php echo $feedback;?>
+</div>
+
+
+<div class="NCcontent">
+<br></br>
+	<p>Här fyller du i dina uppgifter. Sedan är du redo att börja handla!</p><br>
+		
+		<form action="createMember.php" method="POST">
+			<p><label for="Fname" for ="Fname">Förnamn:</label>
+			&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;<input type="text" type="text" id="Fname" name="Fname" value="" /><br><br>
+			<label for="Lname" for ="Lname">Efternamn:</label>
+			&nbsp; &nbsp; &nbsp; <input type="text" id="Lname" name="Lname" value=""/><br><br>
+			 <label for="Address" for ="Address">Adress:</label>
+			&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;<input type="text" id="Address" name="Address" value=""/><br><br>
+			 <label for="Zip" for ="Zip">Postnummer:</label>
+			 <input type="text" id="Zip" name="Zip" value=""/><br><br>
+			 <label for="Email" for ="Email">Din E-post:</label>
+			&nbsp; &nbsp; &nbsp; <input type="text" id="Email" name="Email" value=""/><br><br>
+			 <label for="Password" for ="Password">Ditt lösenord:</label>
+			 <input type="Password" id="Password" name="Password" value=""/><br><br></p>
+			<button class="login" id="login"><p>Klar</p></button>
+		</form>
+	</div>
+
+	
+<?php include("footer.php"); ?>
--- myPage.php
+++ myPage.php
@@ -1,60 +1,112 @@
 <?php
 
-$currentPage="myPage";
-
 include("database.php");
-include("header.php"); ?>
+include("header.php");
+$currentPage="myPage";
+$tableOrder= "order";
+$feedback = '';
 
-<div class="content">
-
-<p style="text-align:center"> <br><br><br> Här hittar du dina beställningar, dina uppgifter, och allt annat som rör ditt medlemskap! </br><p>
-
-</br><p class="ChangeText"> Här kan du ändra dina uppgifter, om du exempelvis flyttat.</br> Bara fyll i de nya uppgifterna och klicka på klar!</p>
-<br>
- <form action="myPage.php" method="post" id="changeID-form" style="margin-left:200px; margin-right:auto;">
- 		<p><label for ="Fname">Förnamn:</label></p>
- 			<input type="text" id="Fname" name="Fname" value="" /><br>
- 			<p><label for ="Lname">Efternamn:</label></p>
- 			<input id="Lname" name="Lname" value=""/><br>
-  			<p><label for ="Address">Adress:</label></p>
- 			<input type="text" id="Address" name="Address" value="" /><br>
-  			<p><label for ="Zip">Postnummer:</label></p>
- 			<input type="number" id="Zip" name="Zip" value="" /><br>
- 			<p><label for ="Email">E-post:</label></p>
- 			<input type="text" id="Email" name="Email" value="" /><br>
- 			<button class="login" style="margin-left:200px; margin-right:auto;" id="login"><p>KLAR</p></button>
- 		</form>
-<?php
-	if(isset($_POST['gem']))
-{
-
-$Fname = $_POST['Fname'];
-$Lname = $_POST['Lname'];
-$Address = $_POST['Address'];
-$Zip = $_POST['Zip'];
-$Email = $_POST['Email'];
-
-$query = <<<END
-	
-	UPDATE * 
-	FROM {$table} 
-	WHERE Email = '{$username}' AND Password = '{$password}'; 
-
+if(!empty($_POST)) {
+        
+		$table = "customer";
+		
+		$Fname =		isset($_POST['Fname']) ? $_POST['Fname'] : ''; 
+		$Lname =		isset($_POST['Lname']) ? $_POST['Lname'] : '';
+		$Address =		isset($_POST['Address']) ? $_POST['Address'] : '';
+		$Zip =			isset($_POST['Zip']) ? $_POST['Zip'] : '';
+		$Email =		isset($_POST['Email']) ? $_POST['Email'] : ''; 
+		$Password =		isset($_POST['Password']) ? $_POST['Password'] : '';
+		
+			if($Fname == '' || $Lname == '' || $Address == '' || $Zip == '' || $Email == ''|| $Password == ''){
+				$feedback = "Fyll i alla fält";
+			}
+			else {
+				$Fname 		= utf8_encode($mysqli->real_escape_string($Fname)); 
+				$Lame 		= utf8_encode($mysqli->real_escape_string($Lname)); 
+				$Address 	= utf8_encode($mysqli->real_escape_string($Address)); 
+				$Zip 		= utf8_encode($mysqli->real_escape_string($Zip)); 
+				$Email		= utf8_encode($mysqli->real_escape_string($Email)); 
+				$Password 	= utf8_encode($mysqli->real_escape_string($Password)); 
+		
+		$query = <<<END
+		UPDATE customer
+		SET Fname='{$Fname}', 
+			Lname='{$Lname}', 
+			Address='{$Address}', 
+			Zip='{$Zip}', 
+			Email='{$Email}', 
+			Password='{$Password}'
+		WHERE CustomerID='7';
 END;
-
-$retval = mysqli_query($con,$sql );
-if(! $retval )
-{
-  die('Could not update data: ' . mysql_error());
-}
-echo "Updated data successfully\n";
-
-
+	
+	$mysqli->query($query) or die("Could not query database" . $mysqli->errno . " : " . $mysqli->error); //Performs query 
+	$feedback = "Dina uppgifter har ändrats";
+	header("Location: myPage.php");
+    }
+	$feedback = "Dina uppgifter har ändrats";
+	$mysqli->close();
 }
 ?>
+
+	<div class="MPcontentOrder">
+	
+	<br></br>
+	<p> Här kan du se dina tidigare beställningar.</p>
 		
-	<p class="PreviousOrder" style="float:right; margin-top:-300px"> Här kommer du se dina tidigare beställningar. </br> Just nu har du inga tidigare ordrar.</p>
+<?php
+/* $result = <<<END
+	--
+	-- Gets all posts from DB
+	--
+	SELECT *
+	FROM {$tableOrder};
+END;
+
+$res = $mysqli->query($result) or die ("Could not query database" . $mysqli->errno ." : " . $mysqli->error);
+
+	while($row = $res->fetch_object()) {
+	$date = strtotime($row->postTimestamp); 
+	$date = date("d M Y H:i", $date); 
+	
+	$OrderID	= utf8_decode(htmlspecialchars($row->OrderID));
+	$Date		= utf8_decode(htmlspecialchars($row->Date));
+	$Amount		= utf8_decode(htmlspecialchars($row->Amount));
+	$Price		= utf8_decode(htmlspecialchars($row->Price));
+
+	$result .= <<<END
+		<div class="MPcontentOrder">
+			<p>Beställt: {$Date}</p>
+			<p>{$OrderID}</p>
+			<p>{$Amount}</p>
+			<p>{$Price}</p>
+		</div>
+END;
+} */
+?>
 		
+<div class="MPcontent">
+
+<br></br>
+
+
+<p> Här kan du ändra dina uppgifter, om du exempelvis flyttat.</br> Bara fyll i de nya uppgifterna och klicka på klar!</p>
+
+ <form action="myPage.php" method="post" id="changeID-form">
+			<p><label for ="Fname">Förnamn:</label>
+ 			&nbsp; &nbsp; &nbsp; <input type="text" id="Fname" name="Fname" value=""/><br></br>
+ 			<label for ="Lname">Efternamn:</label>
+ 			&nbsp; &nbsp; <input id="Lname" name="Lname" value=""/><br></br>
+  			<label for ="Address">Adress:</label>
+ 			&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; <input type="text" id="Address" name="Address" value=""/><br></br>
+  			<label for ="Zip">Postnummer:</label>
+ 			<input type="number" id="Zip" name="Zip" value=""/><br></br>
+ 			<label for ="Email">E-post:</label>
+ 			&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; <input type="text" id="Email" name="Email" value="" /><br></br>
+ 			<label for ="Password">Lösenord:</label>
+ 			&nbsp; &nbsp; &nbsp; <input type="password" id="Password" name="Password" value=""/><br></br></p>
+			<button class="login" style="margin-left:200px; margin-right:auto;" id="login"><p>KLAR</p></button>
+ 		</form>
+	</div>
 </div>
 
 <?php include("footer.php");?>
